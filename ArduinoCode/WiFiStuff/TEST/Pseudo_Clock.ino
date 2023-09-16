5bc6f1ce4ef672ea0e820f480995f811e8537156
SortelOne <Sortel1@yahoo.com>
2023-09-16 17:38:12 -0600

    Start writing pseudo clock code

--- ArduinoCode/WiFiStuff/TEST/Pseudo_Clock.ino
+++ ArduinoCode/WiFiStuff/TEST/Pseudo_Clock.ino
@@ -13,20 +13,91 @@ const char *password = "caNY0u3scAp3?!";
 // THIS IS THE PORT FOR THIS ESP! When you send it requests you have to include the port!
 WiFiServer server(1234);
 
-// Add any variables that you might need here!
+//============= VARIABLES =============
 
-// Current time
-unsigned long currentTime = millis();
-// Previous time
-unsigned long previousTime = 0;
-// Define timeout time in milliseconds (example: 1000ms = 1s)
-const long timeoutTime = 1000;
+//These are for the code so the clock stuff only fires once every second
+unsigned long currentTime = millis(); // Current time
+unsigned long previousTime = 0; // Previous time
+const long timeoutTime = 1000; // Define timeout time in milliseconds (example: 1000ms = 1s)
+
+//VARIABLES NEEDED FOR CLOCK TO FUNCTION
+const int MAXCLOCKVALUE = 3600; //Number of seconds in an hour
+const int JUMPSIZE = 2; //Tells how many time units we are going to advance per timeoutTime.
+int currentGameClockTime = 0; //Current time displayed on the player clock (in seconds)
+/*
+    There are going to be various "modes" that the clock can be in...
+        0: Stationary. Nothing is moving, mostly just for testing (and also for starting)
+        1: Normal operation. This is when the clock is ticking once every "timeoutTime"
+        2: Reverse: While in reverse mode we are going to be moving the servo as fast as possible BACKWARDS
+        3: Fast forward: While in reverse mode we are going to be moving the servo as fast as possible FORWARDS
+*/
+int clockMode = 0;
+
+//============= END VARIABLES =============
+
+//============= HELPER FUNCTIONS =============
+
+//This is where we are going to turn on the stepper motor until we hit the slot interruptor.`
+//For now it just sets the currentGameClockTime value to 0.
+void resetClock(){
+    currentGameClockTime = 0;
+}
+
+string processClientRequest(){
+    // PLACEHOLDER: turns the GPIOs on and off
+    string response;
+
+    if (header.indexOf("GET /?mode=0") >= 0)
+    {
+        mode = 0;
+        //Turn motor off to reset from previous command
+
+        Serial.println("Mode set to 0");
+        response = "{'mode':'0'}";
+    }
+    else if (header.indexOf("GET /?mode=1") >= 0)
+    {
+        mode = 1;
+        //Turn motor off to reset from previous command
+
+        Serial.println("Mode set to 1");
+        response = "{'mode':'1'}";
+    }
+    else if (header.indexOf("GET /?mode=2") >= 0)
+    {
+        mode = 2;
+        //Turn motor off to reset from previous command
+
+        Serial.println("Mode set to 2");
+        response = "{'mode':'2'}";
+    }
+    else if (header.indexOf("GET /?mode=3") >= 0)
+    {
+        mode = 3;
+        //Turn motor off to reset from previous command
+
+        Serial.println("Mode set to 3");
+        response = "{'mode':'3'}";
+    }
+    else{
+        response = "{'command':'unknown'}"
+    }
+
+    return response;
+}
+
+//============= END HELPER FUNCTIONS =============
 
 void setup()
 {
     Serial.begin(115200);
     // Initialize the built-in LED
-    pinMode(LED_BUILTIN, OUTPUT);  // Initialize the LED_BUILTIN pin as an output
+    pinMode(LED_BUILTIN, OUTPUT); 
+    //Turn the LED off, theres no reason for it to be on rn.
+    digitalWrite(LED_BUILTIN, HIGH);
+
+    //Reset the clock
+    resetClock();
 
     // Connect to Wi-Fi network with SSID and password
     Serial.print("Connecting to ");
@@ -69,25 +140,20 @@ void loop()
                     // that's the end of the client HTTP request, so send a response:
                     if (currentLine.length() == 0)
                     {
+                        string response = processClientRequest();
+                        // The HTTP response ends with another blank line
+                        unsigned int responseLength = response.length + 2; //Add two here because we technically need to include the quotation marks
+
                         // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
                         // and a content-type so the client knows what's coming, then a blank line:
                         rcvClient.println("HTTP/1.1 200 OK");
                         rcvClient.println("Content-type: application/json");
-                        rcvClient.println("Content-Length: 20"); //THIS NEEDS TO BE DYNAMIC
+                        rcvClient.println("Content-Length: " + responseLength); //THIS NEEDS TO BE DYNAMIC
                         rcvClient.println("Access-Control-Allow-Origin: *");
                         rcvClient.println();
 
-                        // PUT YOUR LOGIC HERE!
-                        // Change the strings after GET to what youre hoping the URL will have in it
-                        if (header.indexOf("GET /?led=on") >= 0)
-                        {
-                            Serial.println("LED On");
-                            ledStateOfOtherESP = "on";
-                            digitalWrite(LED_BUILTIN, LOW); // Turn the LED on (Note that LOW is the voltage level
-                        }
-
-                        // The HTTP response ends with another blank line
-                        rcvClient.println("{'success':'true'}"); //CONTENT LENGTH DYNAMIC BASED ON HOW LONG THIS IS (IN CHARACTERS) + 1
+                        
+                        rcvClient.println(response);
                         // Break out of the while loop
                         break;
                     }
@@ -111,42 +177,41 @@ void loop()
     }
     else
     {
-        // Once per second I want the "clock" to advance
-        if ((millis() - lastTime) > timerDelay)
-        {
-            // Check WiFi connection status
-            if (WiFi.status() == WL_CONNECTED)
-            {
-                WiFiClient sendClient;
-                HTTPClient http;
-                String serverPath = INSERT_URL_TO_ACCESS_HERE;
-
-                // Your Domain name with URL path or IP address with path
-                http.begin(sendClient, serverPath.c_str());
-
-                // Send HTTP GET request
-                int httpResponseCode = http.GET();
-
-                if (httpResponseCode > 0)
+        //First check for the mode we are currently in
+        switch(mode){
+            //Stationary
+            case 0:
+                //Stop motor movement.
+            break;
+            //Normal Operation
+            case 1:
+                // Once per second I want the "clock" to advance
+                if ((millis() - lastTime) > timerDelay)
                 {
-                    Serial.print("HTTP Response code: ");
-                    Serial.println(httpResponseCode);
-                    String payload = http.getString();
-                    Serial.println(payload);
+                    //First, check clock timer to make sure its within the interval range.
+                    if(0 <= currentGameClockTime < MAXCLOCKVALUE){
+                        currentGameClockTime += JUMPSIZE;
+                    }
+                    else{
+                        //Stop all motor movement, were where we shouldnt be. I dont think this will happen, but good to prepare.
+                    }
+                    //Then, we have to send messages to all of the WiFi modules that need to know what time it is in-game
+
+                    //END: Reset the lastTime so the if block doesnt just keep getting hit every cycle
+                    lastTime = millis();
                 }
-                else
-                {
-                    Serial.print("Error code: ");
-                    Serial.println(httpResponseCode);
-                }
-                // Free resources
-                http.end();
-            }
-            else
-            {
-                Serial.println("WiFi Disconnected");
-            }
-            lastTime = millis();
+            break;
+            //Reverse
+            case 2:
+                //Turn motor on backwards
+            break;
+            //Fast Forward
+            case 3:
+                //Turn motor on forwards
+            break;
+            default:
+                //Stop motor movement again? Idk what the default should be.
+            break;
         }
     }
 }
