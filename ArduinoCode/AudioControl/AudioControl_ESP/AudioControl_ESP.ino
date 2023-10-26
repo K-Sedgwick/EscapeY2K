c5b4add9a38e0fc0ed221686124dd43486185cdc
Jake Bales <sortel1@yahoo.com>
2023-10-25 20:04:34 -0600

    Prototype for the AudioControl ESP module

--- ArduinoCode/AudioControl/AudioControl_ESP/AudioControl_ESP.ino
+++ ArduinoCode/AudioControl/AudioControl_ESP/AudioControl_ESP.ino
@@ -1,5 +1,7 @@
-// Load Wi-Fi library
+// Load Required libraries
+#include <string.h>
 #include <ESP8266WiFi.h>
+#include <SoftwareSerial.h>
 
 // ---- WIFI SECTION ----
 const char *ssid = "EscapeY2K";//EscapeY2K
@@ -11,18 +13,53 @@ unsigned long currentTime = millis();
 unsigned long previousTime = 0;
 const long TickDelayTime = 500;
 
+// ---- MP3 SECTION ----
+// Define the RX and TX pins to establish UART communication with the MP3 Player Module.
+#define MP3_RX 3 // to TX
+#define MP3_TX 1 // to RX
+
+// Define the required MP3 Player Commands:
+
+// Select storage device to TF card
+static int8_t select_SD_card[] = {0x7e, 0x03, 0X35, 0x01, 0xef}; // 7E 03 35 01 EF
+// Play with index: /01/00{SONG_NUM_HERE}xxx.mp3
+static int8_t play_default_song[] = {0x7e, 0x04, 0x41, 0x00, 0x01, 0xef}; // 7E 04 41 00 01 EF
+// Play the song.
+static int8_t play[] = {0x7e, 0x02, 0x01, 0xef}; // 7E 02 01 EF
+// Pause the song.
+static int8_t pause[] = {0x7e, 0x02, 0x02, 0xef}; // 7E 02 02 EF
+// + Volume
+static int8_t volUp[] = {0x7e, 0x02, 0x05, 0xef}; // 7E 02 02 EF
+// - Volume
+static int8_t volDown[] = {0x7e, 0x02, 0x06, 0xef}; // 7E 02 02 EF
+
+// Define the Serial MP3 Player Module.
+SoftwareSerial MP3(MP3_RX, MP3_TX);
+
 // ---- SETUP AND LOOP ----
 
 void setup()
 {
 	// start serial connection
-	Serial.begin(115200);
-
 	connectToWifi();
 
-  //Setup pin for LED so we can test stuff
-  pinMode(LED_BUILTIN, OUTPUT); 
-  digitalWrite(LED_BUILTIN, LOW); // Turn the LED off
+	//Setup serial communication
+	Serial.begin(9600);
+
+	// Initiate the Serial MP3 Player Module.
+  	MP3.begin(9600);
+	// Select the SD Card.
+  send_command_to_MP3_player(select_SD_card, 5);
+	//And turn the volume down because I think it starts at max volume
+	send_command_to_MP3_player(volDown, 4);
+	send_command_to_MP3_player(volDown, 4);
+	send_command_to_MP3_player(volDown, 4);
+	send_command_to_MP3_player(volDown, 4);
+	send_command_to_MP3_player(volDown, 4);
+
+	//Setup pin for LED so we can test stuff
+	pinMode(LED_BUILTIN, OUTPUT); 
+	digitalWrite(LED_BUILTIN, LOW); // Turn the LED off
 }
 
 void loop()
@@ -69,13 +106,31 @@ void handleClientConnected(WiFiClient rcvClient)
 				{
 					String fullMessage = "{";
 
-					if (header.indexOf("GET /?cmd1=true") >= 0)
+					if (header.indexOf("GET /?play=#") >= 0)
 					{
-            //INCLUDE WHAT YOU WANT THIS COMMAND TO DO HERE
+						//TODO: Break up the string, parse the val sent as an int, then send that value over serial
+						int songNum = header[header.indexOf("GET /?song=play")+1];
+						static int8_t play_selected_song[6];
+						memcpy(play_selected_song, play_default_song, sizeof(play_default_song));
+						play_selected_song[4] = songNum;
+						send_command_to_MP3_player(play_selected_song, 6);
 					}
-					else if (header.indexOf("GET /?cmd2=true") >= 0)
+          //This play is different, it just plays from where we paused instead of restarting the track
+          else if (header.indexOf("GET /?song=play") >= 0)
 					{
-            //INCLUDE WHAT YOU WANT THIS COMMAND TO DO HERE
+  						send_command_to_MP3_player(play, 4);
+					}
+					else if (header.indexOf("GET /?song=pause") >= 0)
+					{
+  						send_command_to_MP3_player(pause, 4);
+					}
+					else if (header.indexOf("GET /?vol=up") >= 0)
+					{
+						send_command_to_MP3_player(volUp, 4);
+					}
+					else if (header.indexOf("GET /?vol=down") >= 0)
+					{
+						send_command_to_MP3_player(volDown, 4);
 					}
 
           //This allows us to add any other properties we may want to add and then still close the response when were done
@@ -110,6 +165,12 @@ void handleClientConnected(WiFiClient rcvClient)
 	rcvClient.stop();
 	Serial.println("Client disconnected.");
 	Serial.println("");
+}
+
+void send_command_to_MP3_player(int8_t command[], int len){
+  Serial.print("\nMP3 Command => ");
+  for(int i=0;i<len;i++){ MP3.write(command[i]); Serial.print(command[i], HEX); }
+  delay(1000);
 }
 
 void connectToWifi()
