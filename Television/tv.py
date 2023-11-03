2c8382bc1901b96090f8ffa8678dd9b63ad0faa9
Nami Eskandarian <105840905+NamiEsk1@users.noreply.github.com>
2023-11-02 23:17:15 -0600

    Videos of escape room playing through hotkeys

--- Television/tv.py
+++ Television/tv.py
@@ -1,16 +1,20 @@
+import sys
 import vlc
 import keyboard
+from time import sleep
  
 # Creating a vlc instance
 vlc_instance = vlc.Instance()
  
 # Media
-earth = vlc_instance.media_new("C:/Users/EscapeY2K/Videos/Samples/Earth.mov")
-rain = vlc_instance.media_new("C:/Users/EscapeY2K/Videos/Samples/Rain.mp4")
-lions = vlc_instance.media_new("C:/Users/EscapeY2K/Videos/Samples/Lions.mp4")
-yoga = vlc_instance.media_new("C:/Users/EscapeY2K/Videos/Samples/Yoga.mp4")
+dark = vlc.Media("C:/Users/EscapeY2K/Videos/EscapeY2K/dark.mp4")
+timetravel = vlc.Media("C:/Users/EscapeY2K/Videos/EscapeY2K/timetravel.mp4")
+game = vlc.Media("C:/Users/EscapeY2K/Videos/EscapeY2K/game.mp4")
+monster = vlc.Media("C:/Users/EscapeY2K/Videos/EscapeY2K/monster.mp4")
+midnight = vlc.Media("C:/Users/EscapeY2K/Videos/EscapeY2K/midnight.mp4")
+ending = vlc.Media("C:/Users/EscapeY2K/Videos/EscapeY2K/ending.mp4")
 
-playlist = vlc.MediaList([earth, rain, lions, yoga])
+playlist = vlc.MediaList([dark, timetravel, game, monster, midnight, ending])
     
 # Creating a media list player
 list_player = vlc.MediaListPlayer(vlc_instance)
@@ -19,15 +23,21 @@ list_player.set_media_list(playlist)
 # Setting up regular media player
 media_player = list_player.get_media_player()
 media_player.toggle_fullscreen()
+
+def switch_video(video, hotkey):
+    list_player.play_item(timetravel)
+    while keyboard.is_pressed(hotkey): pass
+    list_player.play_item(video)
     
 # Play the video
-list_player.play()
+list_player.play_item(dark)
 list_player.set_playback_mode(vlc.PlaybackMode().repeat)
 
-keyboard.add_hotkey('e', lambda: list_player.play_item_at_index(0))
-keyboard.add_hotkey('r', lambda: list_player.play_item_at_index(1))
-keyboard.add_hotkey('l', lambda: list_player.play_item_at_index(2))
-keyboard.add_hotkey('y', lambda: list_player.play_item_at_index(3))
+keyboard.add_hotkey('d', switch_video, args = (dark, 'd'))
+keyboard.add_hotkey('g', switch_video, args = (game, 'g'))
+keyboard.add_hotkey('m', switch_video, args = (monster, 'm'))
+keyboard.add_hotkey('f', switch_video, args = (midnight, 'f'))
+keyboard.add_hotkey('e', switch_video, args = (ending, 'e'))
 
 keyboard.add_hotkey('0', lambda: media_player.set_position(0))
 keyboard.add_hotkey('1', lambda: media_player.set_position(0.1))
@@ -40,6 +50,7 @@ keyboard.add_hotkey('7', lambda: media_player.set_position(0.7))
 keyboard.add_hotkey('8', lambda: media_player.set_position(0.8))
 keyboard.add_hotkey('9', lambda: media_player.set_position(0.9))
 
-# Program exits when escape is pressed
-keyboard.wait('esc')
-list_player.stop()
+while (not keyboard.is_pressed('esc')):
+    # The midnight video needs to loop further into the video
+    if (media_player.get_media().get_mrl() == midnight.get_mrl()):
+        if (media_player.get_position() > 0.9): media_player.set_position(0.1)
